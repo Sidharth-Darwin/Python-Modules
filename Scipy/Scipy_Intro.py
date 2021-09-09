08eb920c9fba3ecc251039fd87cecdde4338bdcd
BruhOtaku <zurajanaikatsurada1222@gmail.com>
2021-09-09 12:03:15 +0530

    Created scipy intro

---
+++ Scipy/Scipy_Intro.py
@@ -0,0 +1,190 @@
+
+# How many cubic meters are in one liter:
+# from scipy import constants
+# print(constants.liter)
+
+# constants: SciPy offers a set of mathematical constants, 
+#   one of them is liter which returns 1 liter as cubic meters.
+
+##############################################################
+
+# check version
+# import scipy
+# print(scipy.__version__)
+
+##############################################################
+
+# As SciPy is more focused on scientific implementations, it provides many built-in scientific constants.
+# These constants can be helpful when you are working with Data Science.
+
+# Print the constant value of PI:
+# from scipy import constants
+# print(constants.pi)
+
+##############################################################
+
+# Constant Units
+# List all constants:
+# from scipy import constants
+# print(dir(constants))
+
+##############################################################
+
+# Unit Categories
+# The units are placed under these categories:
+# •	Metric
+# •	Binary
+# •	Mass
+# •	Angle
+# •	Time
+# •	Length
+# •	Pressure
+# •	Volume
+# •	Speed
+# •	Temperature
+# •	Energy
+# •	Power
+# •	Force
+
+##############################################################
+
+# Metric (SI) Prefixes:
+# Return the specified unit in meter (e.g. centi returns 0.01)
+
+# from scipy import constants
+# print(constants.yotta)    
+# print(constants.zetta)    
+# print(constants.exa)      
+# print(constants.peta)     
+
+##############################################################
+
+# Binary Prefixes:
+# Return the specified unit in bytes (e.g. kibi returns 1024)
+
+# from scipy import constants
+# print(constants.kibi)    
+# print(constants.mebi)    
+# print(constants.gibi)    
+
+##############################################################
+
+# Mass:
+# Return the specified unit in kg (e.g. gram returns 0.001)
+
+# from scipy import constants
+# print(constants.gram)        
+# print(constants.metric_ton) 
+# print(constants.grain)       
+
+##############################################################
+
+# Angle:
+# Return the specified unit in radians (e.g. degree returns 0.017453292519943295)
+
+# from scipy import constants
+# print(constants.degree)     
+# print(constants.arcmin)    
+# print(constants.arcminute) 
+
+##############################################################
+
+# Time:
+# Return the specified unit in seconds (e.g. hour returns 3600.0)
+
+# from scipy import constants
+# print(constants.minute)      
+# print(constants.hour)        
+# print(constants.day)         
+# print(constants.week)        
+
+##############################################################
+
+# Length:
+# Return the specified unit in meters (e.g. nautical_mile returns 1852.0)
+
+# from scipy import constants
+# print(constants.inch)              
+# print(constants.foot)              
+# print(constants.yard)              
+# print(constants.mile)              
+
+##############################################################
+
+# Pressure:
+# Return the specified unit in pascals (e.g. psi returns 6894.757293168361)
+
+# from scipy import constants
+# print(constants.atm)         
+# print(constants.atmosphere)  
+# print(constants.bar)       
+
+##############################################################
+
+# Area:
+# Return the specified unit in square meters(e.g. hectare returns 10000.0)
+
+# from scipy import constants
+# print(constants.hectare) 
+# print(constants.acre)    
+
+##############################################################
+
+# Volume:
+# Return the specified unit in cubic meters (e.g. liter returns 0.001)
+
+# from scipy import constants
+# print(constants.liter) 
+# print(constants.gallon)            
+# print(constants.barrel)            
+
+###############################################################
+
+# # Speed:
+# # Return the specified unit in meters per second (e.g. speed_of_sound returns 340.5)
+
+# from scipy import constants
+# print(constants.kmh)        
+# print(constants.mph)            
+# print(constants.mach)
+
+##############################################################
+
+# Temperature:
+# Return the specified unit in Kelvin (e.g. zero_Celsius returns 273.15)
+
+# from scipy import constants
+# print(constants.zero_Celsius)      
+# print(constants.degree_Fahrenheit) 
+
+##############################################################
+
+# Energy:
+# Return the specified unit in joules (e.g. calorie returns 4.184)
+
+# from scipy import constants
+# print(constants.eV)            
+# print(constants.electron_volt) 
+# print(constants.calorie)       
+
+##############################################################
+
+# Power:
+# Return the specified unit in watts (e.g. horsepower returns 745.6998715822701)
+
+# from scipy import constants
+# print(constants.hp)         
+# print(constants.horsepower) 
+
+##############################################################
+
+# Force:
+# Return the specified unit in newton (e.g. kilogram_force returns 9.80665)
+
+# from scipy import constants
+# print(constants.dyn)             
+# print(constants.dyne)            
+# print(constants.lbf)  
+
+##############################################################
+##############################################################
